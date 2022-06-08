c9c54a454d68566aa0a1993fbad3bcbe0c2a4942
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-06-08 17:06:02 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+function mondayWork(activity1 = 'go to the office'){
+    return `This Monday, I will ${activity1}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(quo = "*"){
+    return function(adjective = 'awesome'){
+        return `You are ${quo}${adjective}${quo}!`
+    }
+}
